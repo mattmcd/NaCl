db1bf883e2c1ac6015983670bc234cd26d52d39c
mattmcd <matt@matt-mcdonnell.com>
2013-11-24 13:55:12 +0000

    Added bar chart

--- MonteCarlo/mcView.js
+++ MonteCarlo/mcView.js
@@ -42,6 +42,7 @@ function handleMessage(message_event) {
     + " +/- " + res[res.length-1].StdError.toFixed(7) 
     +  " after " + tDiff + "ms" + " for " + nPtsSim + " points" );
   updateTable( res );
+  updatePlot( res );
   var go = document.getElementById( "go" );
   go.disabled = false;
 }
@@ -70,4 +71,23 @@ function updateTable( res ) {
       return [d.Samples, d.Total, d.Mean.toFixed(7), d.StdError.toFixed(7)]; })
     .enter().append("td");
   td.text( function (d) { return d; });
+}
+
+function updatePlot( res ){
+  d3.select("#plot svg").remove();
+  var w=320, h=200;
+  var x = d3.scale.linear().domain([0, res[res.length-1].Samples]).range( [0, w]);
+  var y = d3.scale.linear().domain([0, 1]).range([0, h]);
+  var chart = d3.select("#plot").append("svg").attr("width", w).attr("height", h);
+  var rect = chart.selectAll("rect").data( res );
+  var barWidth = 10;
+  rect.enter()
+    .append("rect")
+    .attr("x", function (d,i) { return x(d.Samples) - barWidth;})
+    .attr("y", function (d,i) { return h - y(d.Mean + d.StdError/2); })
+    .attr("width", function (d,i) { return barWidth; })
+    .attr("height", function (d,i) { return Math.max( y( d.StdError ), 1); })
+    .style("fill", "steelblue");
+  chart.append("line").attr("x1", 0).attr("x2",w).attr("y1",h).attr("y2",h).style("stroke","#000000");
+  chart.append("line").attr("x1", 0).attr("x2",0).attr("y1",h).attr("y2",0).style("stroke","#000000");
 }
