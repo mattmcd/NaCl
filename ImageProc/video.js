e3eaa9957e9f6919095295f6c3bd9103897488ba
mattmcd <matt@matt-mcdonnell.com>
2014-03-10 19:27:49 +0000

    Copy video frames to canvas every 20ms.

--- ImageProc/video.js
+++ ImageProc/video.js
@@ -1,12 +1,20 @@
 function pageDidLoad() {
-  video = document.getElementById("live");
-  var ctx;
+  var video = document.getElementById("live");
+  var display = document.getElementById("display");
+  var context = display.getContext("2d");
+
   navigator.webkitGetUserMedia(
     {video:true, audio:false}, 
     function (stream) {
       video.src = window.URL.createObjectURL(stream);
+      draw( video, context);
     },
     function (err) {
       console.log("Unable to get media stream:" + err.Code );
     });
+}
+
+function draw(v,c) {
+  c.drawImage(v, 0, 0);
+  setTimeout( draw, 20, v, c); // Redraw every 20ms
 }
