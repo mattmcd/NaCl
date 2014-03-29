bbed6a8364dc380832b75f1890cc8b8724108298
mattmcd <matt@matt-mcdonnell.com>
2014-03-29 11:57:31 +0000

    Simple invert processor.

--- ImageProc/processor_invert.cpp
+++ ImageProc/processor_invert.cpp
@@ -3,7 +3,12 @@
 namespace {
   auto invertReg = ObjectRegister<std::function<cv::Mat(cv::Mat)> >("Invert", 
   [](cv::Mat im){ 
-      cv::rectangle(im, cv::Point(100,100), cv::Point(200,200), cv::Scalar(255,0,0), 1);
-      return im; });
+    cv::Mat dest;
+    // Subtract the image from a constant 255 4-channel array
+    cv::subtract( cv::Scalar(255, 255, 255, 255), im, dest );
+    // Set the alpha channel back to fully opaque
+    cv::add( cv::Scalar(0,0,0,255), dest, im );
+    
+    return im; });
 }
 
