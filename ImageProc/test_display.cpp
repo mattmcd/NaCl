b11bf4aac7724a47835300d43ac9b4b4528cbd7c
mattmcd <matt@matt-mcdonnell.com>
2014-12-20 13:26:16 +0000

    Color conversion for test framework.

--- ImageProc/test_display.cpp
+++ ImageProc/test_display.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 int main(int argc, char* argv[])
 {
@@ -16,11 +17,17 @@ int main(int argc, char* argv[])
     std::cout << "Could not read image " << fname << std::endl;
     return -1;
   }
+  // Processors expect 8UC4
+  cv::Mat im;
+  cv::cvtColor( input, im, CV_BGR2RGBA );
   
+  // Use first processor
   auto processorFactory = SingletonFactory<std::function<std::unique_ptr<Processor>()>>::getInstance();
   auto names = processorFactory.getNames();
   auto processor = processorFactory.getObject( names[0] )();
-  auto output = (*processor)( input.clone() );
+  auto output = (*processor)( im );
+  
+  cv::cvtColor( output, output, CV_RGBA2BGR );
   
   cv::namedWindow( "Input", CV_WINDOW_AUTOSIZE );
   cv::namedWindow( "Processed", CV_WINDOW_AUTOSIZE );
